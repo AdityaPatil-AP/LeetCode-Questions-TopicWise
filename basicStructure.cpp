94f1e2bc81480c7d917b2b1df492d556762e45ff
AdityaPatil-AP <adityapatil15963@gmail.com>
2022-07-24 18:38:58 +0530

    Update

---
+++ basicStructure.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+#include <climits>
+#include <cmath>
+using namespace std;
+
+int main(){
+    #ifndef ONLINE_JUDGE
+    freopen("E:\\only one folder\\Cp Codes 1\\Cp Codes\\CpCourseCodesGithupRepo\\Cp\\input.txt","r", stdin);
+    freopen("E:\\only one folder\\Cp Codes 1\\Cp Codes\\CpCourseCodesGithupRepo\\Cp\\output.txt", "w", stdout);
+    #endif
+
+
+
+    return 0;
+}
