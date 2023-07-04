c45d3d028cc07678f1d489d29bcf9cdb22a077d6
AdityaPatil-AP <aditya.patil20@vit.edu>
2023-07-04 19:18:01 +0530

    commit

--- Leet_Code_Solutions/BitManipulation/260.single-number-III.cpp
+++ Leet_Code_Solutions/BitManipulation/260.single-number-III.cpp
@@ -15,6 +15,7 @@ public:
         // return ans;
 
         // Optimised using Bit Manipulation.
+        // Find the xor of the whole array.
         long long number = 0;
         int n = nums.size();
         for (int i = 0; i < n; i++)
@@ -22,6 +23,8 @@ public:
             number ^= nums[i];
         }
 
+        // There is atleast one bit that is the different between
+        // and b. Because they are distinct integers.
         long setBit = 1;
         while (true)
         {
@@ -32,6 +35,11 @@ public:
             setBit <<= 1;
         }
 
+        // Let's say the setBit was present in b, then
+        // we will take all the elements with that bit set for computation
+        // of b. And Obviously that would not contain a. All other elements
+        // will go to computation of a. Hence the repeated elements will cancel
+        // out and we will be remaining with our Final Answers a and b.
         int firstAns = 0, secondAns = 0;
         for (int i = 0; i < n; i++)
         {
