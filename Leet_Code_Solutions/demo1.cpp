c9a9cb5d96ae817aef27f59afa71a27462edb5e5
AdityaPatil-AP <aditya.patil20@vit.edu>
2023-07-10 22:11:32 +0530

    commit

--- Leet_Code_Solutions/demo1.cpp
+++ Leet_Code_Solutions/demo1.cpp
@@ -1,68 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-std::vector<std::string> getWordsFromString(const std::string &text)
-{
-    std::istringstream iss(text);
-    std::vector<std::string> words;
-    std::string word;
-
-    while (iss >> word)
-    {
-        words.push_back(word);
-    }
-
-    return words;
-}
-
-vector<string> getWordsFromString(string &text)
-{
-    istringstream iss(text);
-    vector<string> words;
-    string word;
-
-    while (iss >> word)
-    {
-        words.push_back(word);
-    }
-
-    return words;
-}
-
-vector<string> helper(string &text, int k)
-{
-    vector<string> words = getWordsFromString(text);
-
-    map<string, int> mp;
-    for (auto word : words)
-    {
-        mp[word]++;
-    }
-
-    vector<string> ans;
-    for (auto word : words)
-    {
-        if (mp[word] >= k)
-        {
-            ans.push_back(word);
-            mp[word] = 0;
-        }
-    }
-
-    return ans;
-}
-
 int main()
 {
-    // string text = "hello the a a the the is ohkay ohkay hello";
-    // vector<string> ans = helper(text, 2);
+    vector<string> another;
+    int count = 1;
+    int n = 4;
+    for (int i = 0; i < n; i++)
+    {
+        string curr = "";
+        for (int j = 0; j < (2 * i + 1); j++)
+        {
+            if (j % 2 == 1)
+            {
+                curr += '*';
+            }
+            else
+            {
+                curr += to_string(count);
+                count++;
+            }
+        }
+        cout << curr << endl;
+        another.push_back(curr);
+    }
 
-    // for (auto i : ans)
-    // {
-    //     cout << i << " ";
-    // }
-    // cout << endl;
-
-    cout << (1 << 0) << endl;
+    for (int i = n - 1; i >= 0; i--)
+    {
+        cout << another[i] << endl;
+    }
     return 0;
 }
