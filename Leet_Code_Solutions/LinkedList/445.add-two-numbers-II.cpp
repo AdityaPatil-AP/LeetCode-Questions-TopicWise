a8caf59ed19fd35674573d317baeeb710de513d9
AdityaPatil-AP <aditya.patil20@vit.edu>
2023-07-18 12:31:07 +0530

    commit

--- Leet_Code_Solutions/LinkedList/445.add-two-numbers-II.cpp
+++ Leet_Code_Solutions/LinkedList/445.add-two-numbers-II.cpp
@@ -24,37 +24,78 @@ public:
     ListNode *addTwoNumbers(ListNode *l1, ListNode *l2)
     {
 
-        ListNode *r1 = reverseList(l1);
-        ListNode *r2 = reverseList(l2);
+        // Using Stack as Well.
+        stack<int> s1, s2;
 
-        // Simple sum.
-        ListNode *ans = new ListNode();
+        while (l1)
+        {
+            s1.push(l1->val);
+            l1 = l1->next;
+        }
+        while (l2)
+        {
+            s2.push(l2->val);
+            l2 = l2->next;
+        }
+
         int carry = 0;
-
-        while (r1 != NULL || r2 != NULL || carry)
+        ListNode *ans = new ListNode();
+        while (!s1.empty() || !s2.empty())
         {
             int totalSum = 0;
-            if (r1)
+            if (!s1.empty())
             {
-                totalSum += r1->val;
-                r1 = r1->next;
+                totalSum += s1.top();
+                s1.pop();
             }
-            if (r2)
+            if (!s2.empty())
             {
-                totalSum += r2->val;
-                r2 = r2->next;
+                totalSum += s2.top();
+                s2.pop();
             }
 
             totalSum += carry;
 
-            carry = totalSum / 10;
             int curr = totalSum % 10;
-            ListNode *head = new ListNode();
+            carry = totalSum / 10;
+
+            ListNode *head = new ListNode(carry);
             ans->val = curr;
             head->next = ans;
             ans = head;
         }
 
-        return ans->next;
+        return (carry == 0) ? ans->next : ans;
+
+        // Reversing the Linked List.
+        // ListNode* r1 = reverseList(l1);
+        // ListNode* r2 = reverseList(l2);
+
+        // // Simple sum.
+        // ListNode* ans = new ListNode();
+        // int carry = 0;
+
+        // while(r1 != NULL || r2 != NULL || carry){
+        //     int totalSum = 0;
+        //     if(r1){
+        //         totalSum += r1->val;
+        //         r1 = r1->next;
+        //     }
+        //     if(r2){
+        //         totalSum += r2->val;
+        //         r2 = r2->next;
+        //     }
+
+        //     totalSum += carry;
+
+        //     carry = totalSum/10;
+        //     int curr = totalSum % 10;
+        //     ListNode* head = new ListNode();
+        //     ans->val = curr;
+        //     head->next = ans;
+        //     ans = head;
+        // }
+
+        // return ans->next;
     }
 };
