a9f56243eef2e405d9e237336fc6f55f2bb40762
AdityaPatil-AP <aditya.patil20@vit.edu>
2023-07-26 11:04:08 +0530

    commit

--- Leet_Code_Solutions/BinarySearch/1870.minimum-speed-to-arrive-on-time.cpp
+++ Leet_Code_Solutions/BinarySearch/1870.minimum-speed-to-arrive-on-time.cpp
@@ -1,31 +1,32 @@
 class Solution
 {
 public:
-    bool calc(int mid, vector<int> &dist, double hour)
+    bool calc(int speed, vector<int> &dist, double hour)
     {
         double time = 0;
         int n = dist.size();
         for (int i = 0; i < n - 1; i++)
         {
-            time += ceil((double)dist[i] / (double)mid);
+            time += ceil((double)dist[i] / speed);
         }
-        time += ((double)dist[n - 1] / (double)mid);
-        return (time <= hour) ? true : false;
+        time += (double)dist[n - 1] / speed;
+        return (time <= hour);
     }
 
     int minSpeedOnTime(vector<int> &dist, double hour)
     {
-        // This minimum speed can be anything between 0 and 1e7.
-        // So applying Binary Search as we can divide the search spaces.
+        // This minimum speed can be anything between 0 and 1e7
+
         int low = 1, high = 1e7;
-        if ((double)dist.size() > hour + 1)
+
+        if ((double)dist.size() > ceil(hour))
         {
             return -1;
         }
         int ans = -1;
         while (low <= high)
         {
-            int mid = low + (high - low) / 2;
+            int mid = (low + (high - low) / 2);
             if (calc(mid, dist, hour))
             {
                 ans = mid;
@@ -36,6 +37,7 @@ public:
                 low = mid + 1;
             }
         }
+
         return ans;
     }
 };
