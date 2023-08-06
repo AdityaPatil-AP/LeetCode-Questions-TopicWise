8b20f596349817d1af73154dce37d7525e81ad1d
AdityaPatil-AP <aditya.patil20@vit.edu>
2023-08-06 22:38:41 +0530

    commit

--- Leet_Code_Solutions/DynamicProgramming/920.number-of-music-playlists.cpp
+++ Leet_Code_Solutions/DynamicProgramming/920.number-of-music-playlists.cpp
@@ -27,3 +27,33 @@ public:
         return (int)dp[goal][n];
     }
 };
+
+class Solution
+{
+public:
+    int numMusicPlaylists(int n, int goal, int k)
+    {
+        const int mod = 1e9 + 7;
+
+        // Initialise the dp table.
+        vector<vector<long>> dp(goal + 1, vector<long>(n + 1, 0));
+
+        dp[0][0] = 1;
+
+        for (int i = 1; i <= goal; i++)
+        {
+            for (int j = 1; j <= min(i, n); j++)
+            {
+                // The i-th song is a new song.
+                dp[i][j] = dp[i - 1][j - 1] * (n - j + 1) % mod;
+                // The i-th song is a song we have played before.
+                if (j > k)
+                {
+                    dp[i][j] = (dp[i][j] + dp[i - 1][j] * (j - k)) % mod;
+                }
+            }
+        }
+
+        return dp[goal][n];
+    }
+};
