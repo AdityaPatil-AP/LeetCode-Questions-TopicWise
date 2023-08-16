405580330df67fc45d647ee15e74767413ec812a
AdityaPatil-AP <aditya.patil20@vit.edu>
2023-08-16 19:03:48 +0530

    commit

--- Leet_Code_Solutions/LinkedList/86.partition-list.cpp
+++ Leet_Code_Solutions/LinkedList/86.partition-list.cpp
@@ -30,3 +30,47 @@ public:
         return before.next;
     }
 };
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution
+{
+public
+    ListNode partition(ListNode head, int x)
+    {
+        ListNode after = new ListNode(0);
+        ListNode before = new ListNode(0);
+
+        ListNode after_curr = after;
+        ListNode before_curr = before;
+
+        while (head != null)
+        {
+            if (head.val < x)
+            {
+                before_curr.next = head;
+                before_curr = head;
+            }
+            else
+            {
+                after_curr.next = head;
+                after_curr = head;
+            }
+
+            head = head.next;
+        }
+
+        after_curr.next = null;
+        before_curr.next = after.next;
+
+        return before.next;
+    }
+}
