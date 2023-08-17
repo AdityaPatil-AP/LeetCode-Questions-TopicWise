c099fa584443422fde3392c5cff953e2c66c3e20
AdityaPatil-AP <aditya.patil20@vit.edu>
2023-08-17 12:36:13 +0530

    commit

--- Leet_Code_Solutions/SlidingWindow/239.sliding-window-maximum.cpp
+++ Leet_Code_Solutions/SlidingWindow/239.sliding-window-maximum.cpp
@@ -38,3 +38,46 @@ public:
         return res;
     }
 };
+
+// In Java.
+
+class Solution
+{
+public
+    int[] maxSlidingWindow(int[] nums, int k)
+    {
+        Deque<Integer> dq = new ArrayDeque<>();
+        List<Integer> res = new ArrayList<>();
+
+        for (int i = 0; i < k; i++)
+        {
+            while (!dq.isEmpty() && nums[i] >= nums[dq.peekLast()])
+            {
+                dq.pollLast();
+            }
+
+            dq.offerLast(i);
+        }
+
+        res.add(nums[dq.peekFirst()]);
+
+        for (int i = k; i < nums.length; i++)
+        {
+            if (dq.peekFirst() == i - k)
+            {
+                dq.pollFirst();
+            }
+
+            while (!dq.isEmpty() && nums[i] >= nums[dq.peekLast()])
+            {
+                dq.pollLast();
+            }
+
+            dq.offerLast(i);
+            res.add(nums[dq.peekFirst()]);
+        }
+
+        // returning the result as an array.
+        return res.stream().mapToInt(i->i).toArray();
+    }
+}
