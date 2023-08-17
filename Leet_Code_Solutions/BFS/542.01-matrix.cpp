3c16fe986c86af85c5fc605a20be96f8fd44a32f
AdityaPatil-AP <aditya.patil20@vit.edu>
2023-08-17 14:37:01 +0530

    commit

--- Leet_Code_Solutions/BFS/542.01-matrix.cpp
+++ Leet_Code_Solutions/BFS/542.01-matrix.cpp
@@ -57,3 +57,45 @@ public:
         return mat;
     }
 };
+
+class Solution {
+    public int[][] updateMatrix(int[][] mat) {
+        if(mat == null || mat.length == 0 || mat[0].length == 0){
+            return new int [0][0];
+        }
+
+        int m = mat.length, n = mat[0].length;
+
+        Queue<int[]> queue = new LinkedList<>();
+
+        int max_value = m + n - 2;
+
+        for(int i = 0;i < m;i++){
+            for(int j = 0;j < n;j++){
+                if(mat[i][j] == 0){
+                    queue.offer(new int[]{i, j});
+                }
+                else{
+                    mat[i][j] = max_value;
+                }
+            }
+        }
+
+        int[][] directions = {{1, 0}, {0, 1}, {0, -1}, {-1, 0}};
+
+        while(!queue.isEmpty()){
+            int[] cell = queue.poll();
+            for(int [] dir : directions){
+                int r = cell[0] + dir[0], c = cell[1] + dir[1];
+
+                if(r >= 0 && r < m && c >= 0 && c < n && mat[r][c] > mat[cell[0]][cell[1]] + 1){
+                    queue.offer(new int[]{r, c});
+                    mat[r][c] = mat[cell[0]][cell[1]] + 1;
+                }
+            }
+        }
+
+        return mat;
+    }
+}
+
