045433e4a79c4ec6e8a69123e85b73c738e34b17
AdityaPatil-AP <aditya.patil20@vit.edu>
2023-08-21 23:00:14 +0530

    commit

--- Leet_Code_Solutions/demo1.cpp
+++ Leet_Code_Solutions/demo1.cpp
@@ -1,37 +1,27 @@
-#include <bits/stdc++.h>
+
 using namespace std;
+#include <bits/stdc++.h>
+bool comp(int &a, int &b)
+{
+    if (a % 2 == 0 && b % 2 == 1)
+    {
+        return true;
+    }
+    if (a % 2 == 0 && b % 2 == 0 && a <= b)
+    {
+        return true;
+    }
+    return false;
+}
+
+vector<int> funcArrange(vector<int> inputArr)
+{
+    sort(inputArr.begin(), inputArr.end(), comp);
+
+    return inputArr;
+}
 
 int main()
 {
-    // vector<string> another;
-    // int count = 1;
-    // int n = 4;
-    // for (int i = 0; i < n; i++)
-    // {
-    //     string curr = "";
-    //     for (int j = 0; j < (2 * i + 1); j++)
-    //     {
-    //         if (j % 2 == 1)
-    //         {
-    //             curr += '*';
-    //         }
-    //         else
-    //         {
-    //             curr += to_string(count);
-    //             count++;
-    //         }
-    //     }
-    //     cout << curr << endl;
-    //     another.push_back(curr);
-    // }
-
-    // for (int i = n - 1; i >= 0; i--)
-    // {
-    //     cout << another[i] << endl;
-    // }
-    int a = 3;
-    int &p = a;
-    p = 4;
-    cout << a << endl;
     return 0;
 }
