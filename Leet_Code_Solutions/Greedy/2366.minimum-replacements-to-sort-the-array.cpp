1e2e10449024442ab19b3f9f1035020eca03d9c5
AdityaPatil-AP <aditya.patil20@vit.edu>
2023-08-30 11:38:28 +0530

    commit

--- Leet_Code_Solutions/Greedy/2366.minimum-replacements-to-sort-the-array.cpp
+++ Leet_Code_Solutions/Greedy/2366.minimum-replacements-to-sort-the-array.cpp
@@ -24,3 +24,35 @@ public:
         return answer;
     }
 };
+
+class Solution
+{
+public
+    long minimumReplacement(int[] nums)
+    {
+        long answer = 0;
+        int n = nums.length;
+
+        // Start from the second last element
+        int last = nums[n - 1];
+        for (int i = n - 2; i >= 0; i--)
+        {
+            if (nums[i] <= last)
+            {
+                last = nums[i];
+            }
+            else
+            {
+                int numElements = nums[i] / last;
+                if (nums[i] % last != 0)
+                    numElements++;
+
+                answer += numElements - 1;
+
+                last = (nums[i] / (numElements));
+            }
+        }
+
+        return answer;
+    }
+}
