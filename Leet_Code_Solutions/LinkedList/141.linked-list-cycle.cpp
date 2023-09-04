3cf0ef3dfe64dcb185dc52aa838b575eb6e87699
AdityaPatil-AP <aditya.patil20@vit.edu>
2023-09-04 13:56:27 +0530

    commit

---
+++ Leet_Code_Solutions/LinkedList/141.linked-list-cycle.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution
+{
+public:
+    bool hasCycle(ListNode *head)
+    {
+        // To check if there is a cycle in single linked list. We have to use slow and fast
+        // pointers.
+
+        ListNode *slow = head;
+        ListNode *fast = head;
+
+        while (fast != NULL && fast->next != NULL)
+        {
+            fast = fast->next->next;
+            slow = slow->next;
+
+            if (fast == slow)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
