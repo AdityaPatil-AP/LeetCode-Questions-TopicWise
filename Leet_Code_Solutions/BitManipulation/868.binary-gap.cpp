8729d2864e8d4c5414136352b1f88ca7e18a5c8f
AdityaPatil-AP <aditya.patil20@vit.edu>
2023-09-26 19:33:38 +0530

    commit

--- Leet_Code_Solutions/BitManipulation/868.binary-gap.cpp
+++ Leet_Code_Solutions/BitManipulation/868.binary-gap.cpp
@@ -19,23 +19,38 @@ public:
 
     int binaryGap(int n)
     {
-        int gap = 0;
+        // String Representation.
+        // int gap = 0;
 
-        int last1 = -1;
-        string num = binaryNumber(n);
-        for (int i = 0; i < num.size(); i++)
+        // int last1 = -1;
+        // string num = binaryNumber(n);
+        // for(int i = 0;i < num.size();i++){
+        //     if(last1 == -1 && num[i] == '1'){
+        //         last1 = i;
+        //     }
+        //     else if(num[i] == '1'){
+        //         gap = max(gap, i - last1);
+        //         last1 = i;
+        //     }
+        // }
+
+        // return gap;
+
+        // Using Bit Manipulation.
+        // Right Shift Operators and Bitwise operators use karke.
+        int last = -1, ans = 0;
+        for (int i = 0; i < 32; i++)
         {
-            if (last1 == -1 && num[i] == '1')
+            if (((n >> i) & 1))
             {
-                last1 = i;
-            }
-            else if (num[i] == '1')
-            {
-                gap = max(gap, i - last1);
-                last1 = i;
+                if (last >= 0)
+                {
+                    ans = max(ans, i - last);
+                }
+                last = i;
             }
         }
 
-        return gap;
+        return ans;
     }
 };
