0555f01dba89f28e15be73deca9583bc17b53773
AdityaPatil-AP <aditya.patil20@vit.edu>
2023-10-08 23:53:40 +0530

    commit

--- Leet_Code_Solutions/DynamicProgramming/1458.maximum-dot-product-of-two-subsequences.cpp
+++ Leet_Code_Solutions/DynamicProgramming/1458.maximum-dot-product-of-two-subsequences.cpp
@@ -51,8 +51,21 @@ public:
             return firstMin * secondMax;
         }
 
-        memo = vector(nums1.size(), vector(nums2.size(), 0));
+        // memo = vector(nums1.size(), vector(nums2.size(), 0));
 
-        return dp(0, 0, nums1, nums2);
+        // return dp(0, 0, nums1, nums2);
+
+        // Another approach.
+        vector<vector<int>> dp = vector(nums1.size() + 1, vector(nums2.size() + 1, 0));
+        for (int i = nums1.size() - 1; i >= 0; i--)
+        {
+            for (int j = nums2.size() - 1; j >= 0; j--)
+            {
+                int use = nums1[i] * nums2[j] + dp[i + 1][j + 1];
+                dp[i][j] = max(use, max(dp[i + 1][j], dp[i][j + 1]));
+            }
+        }
+
+        return dp[0][0];
     }
 };
