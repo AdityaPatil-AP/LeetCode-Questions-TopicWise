6e96e7c83b6b938c183e45f975b8f2cd79a627cf
AdityaPatil-AP <aditya.patil20@vit.edu>
2023-10-10 23:02:17 +0530

    commit

---
+++ Leet_Code_Solutions/DynamicProgramming/1964.find-the-longest-valid-obstacle-course-in-each-position.cpp
@@ -0,0 +1,58 @@
+class Solution
+{
+public:
+    vector<int> longestObstacleCourseAtEachPosition(vector<int> &obstacles)
+    {
+        // Traditional LIS.
+        // int n = obstacles.size();
+        // vector<int> dp(n, 1);
+
+        // for(int i = 1;i < n;i++){
+        //     for(int j = i - 1;j >= 0;j--){
+        //         if(obstacles[j] <= obstacles[i]){
+        //             dp[i] = max(dp[i], 1 + dp[j]);
+        //         }
+        //     }
+        // }
+
+        // return dp;
+
+        // Improved LIS.
+        int n = obstacles.size();
+
+        vector<int> ans(n, 1);
+        // Whenever we have to reduce the time complexity of searching
+        // we have to use some sorted container.
+        // We may also need to use some extra space.
+
+        vector<int> temp;
+        for (int i = 0; i < n; i++)
+        {
+            int low = 0, high = temp.size() - 1;
+            while (low <= high)
+            {
+                int mid = (low + high) / 2;
+                if (temp[mid] > obstacles[i])
+                {
+                    high = mid - 1;
+                }
+                else
+                {
+                    low = mid + 1;
+                }
+            }
+            if (low == temp.size())
+            {
+                temp.push_back(obstacles[i]);
+            }
+            else
+            {
+                temp[low] = obstacles[i];
+            }
+
+            ans[i] = low + 1;
+        }
+
+        return ans;
+    }
+};
