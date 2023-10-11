08a799c0df2b6e7dae8e817c85a6f926099fb7c0
AdityaPatil-AP <aditya.patil20@vit.edu>
2023-10-11 11:18:28 +0530

    commit

--- Leet_Code_Solutions/Sorting/56.merge-intervals.cpp
+++ Leet_Code_Solutions/Sorting/56.merge-intervals.cpp
@@ -29,3 +29,58 @@ public:
         // return res;
     }
 };
+
+class Solution
+{
+public:
+    static bool compareT(vector<int> &p1, vector<int> &p2)
+    {
+        if (p1[0] < p2[0])
+        {
+            return true;
+        }
+        if (p1[0] == p2[0])
+        {
+            if (p1[1] < p2[1])
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    vector<vector<int>> merge(vector<vector<int>> &intervals)
+    {
+        // Merge Intervals.
+        // Merge all the overlapping intervals.
+        // How to sort?
+        // If I sort by the starting time.
+        // We have to sort by the starting time because
+
+        vector<vector<int>> ans;
+
+        sort(intervals.begin(), intervals.end(), compareT);
+
+        int start = intervals[0][0];
+        int end = intervals[0][1];
+        int n = intervals.size();
+
+        for (int i = 1; i < n; i++)
+        {
+            if (end >= intervals[i][0])
+            {
+                end = max(intervals[i][1], end);
+            }
+            else
+            {
+                ans.push_back({start, end});
+                start = intervals[i][0];
+                end = intervals[i][1];
+            }
+        }
+
+        ans.push_back({start, end});
+
+        return ans;
+    }
+};
