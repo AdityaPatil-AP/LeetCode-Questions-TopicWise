986c9577d1529fe7b5e95e817f56b2d95cf31fee
AdityaPatil-AP <aditya.patil20@vit.edu>
2023-10-16 20:13:20 +0530

    commit

--- Leet_Code_Solutions/DynamicProgramming/119.pascal's-triangle-2.cpp
+++ Leet_Code_Solutions/DynamicProgramming/119.pascal's-triangle-2.cpp
@@ -34,3 +34,22 @@ public:
         return currRow;
     }
 };
+
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        vector<int> curr;
+        vector<int> prev;
+        prev.push_back(1);
+
+        for(int i = 1;i <= rowIndex;i++){
+            curr.resize(i + 1, 1);
+            for(int j = 1;j < i;j++){
+                curr[j] = prev[j] + prev[j - 1];
+            }
+            prev = curr;
+        }
+
+        return prev;
+    }
+};
