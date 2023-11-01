857334ecac3b984d20ae9b28966c84315d961c92
AdityaPatil-AP <aditya.patil20@vit.edu>
2023-11-01 23:41:00 +0530

    commit

--- Leet_Code_Solutions/BinarySearchTrees/501.finding-mode-in-binary-search-tree.cpp
+++ Leet_Code_Solutions/BinarySearchTrees/501.finding-mode-in-binary-search-tree.cpp
@@ -113,3 +113,57 @@ public:
         return ans;
     }
 };
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution
+{
+public:
+    void dfs(TreeNode *root, map<int, int> &mp)
+    {
+        if (root == NULL)
+        {
+            return;
+        }
+
+        dfs(root->left, mp);
+        mp[root->val]++;
+        dfs(root->right, mp);
+    }
+
+    vector<int> findMode(TreeNode *root)
+    {
+        map<int, int> mp;
+
+        dfs(root, mp);
+
+        vector<int> ans;
+
+        int mode = 0;
+
+        for (auto it : mp)
+        {
+            if (it.second > mode)
+            {
+                ans.clear();
+                ans.push_back(it.first);
+                mode = it.second;
+            }
+            else if (it.second == mode)
+            {
+                ans.push_back(it.first);
+            }
+        }
+
+        return ans;
+    }
+};
