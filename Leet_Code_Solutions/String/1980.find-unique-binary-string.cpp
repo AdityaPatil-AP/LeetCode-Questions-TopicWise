86f239d2d603efddafdf21a665b57468fef2dd3a
AdityaPatil-AP <aditya.patil20@vit.edu>
2023-11-16 14:02:07 +0530

    commit

--- Leet_Code_Solutions/String/1980.find-unique-binary-string.cpp
+++ Leet_Code_Solutions/String/1980.find-unique-binary-string.cpp
@@ -24,24 +24,52 @@ public:
 
     string findDifferentBinaryString(vector<string> &nums)
     {
-        unordered_set<string> st;
+        // unordered_set<string> st;
 
-        for (string curr : nums)
+        // for(string curr : nums) st.insert(curr);
+
+        // int n = nums.size();
+        // int last = pow(2, n);
+
+        // for(int i = 0;i < last;i++){
+        //     string curr = generate(i, n);
+
+        //     if(st.find(curr) == st.end()){
+        //         return curr;
+        //     }
+        // }
+
+        // return "-1";
+
+        unordered_set<int> st;
+        for (string num : nums)
+        {
+            int curr = generate(num);
             st.insert(curr);
+        }
 
         int n = nums.size();
         int last = pow(2, n);
-
         for (int i = 0; i < last; i++)
         {
-            string curr = generate(i, n);
-
-            if (st.find(curr) == st.end())
+            if (st.find(i) == st.end())
             {
-                return curr;
+                return generate(i, n);
             }
         }
 
         return "-1";
+    }
+
+    int generate(string curr)
+    {
+        int num = 0;
+        int n = curr.size();
+        for (int i = n - 1; i >= 0; i--)
+        {
+            num += (curr[i] == '1') ? pow(2, n - i - 1) : 0;
+        }
+
+        return num;
     }
 };
