8c66e342834cda1c078c7320f415d0f1b8817248
AdityaPatil-AP <aditya.patil20@vit.edu>
2023-11-16 22:50:02 +0530

    commit

--- Leet_Code_Solutions/Priority_Queue/1921.eliminate-maximum-number-of-monsters.cpp
+++ Leet_Code_Solutions/Priority_Queue/1921.eliminate-maximum-number-of-monsters.cpp
@@ -1,26 +1,48 @@
 class Solution {
 public:
     int eliminateMaximum(vector<int>& dist, vector<int>& speed) {
-        double t = 0;
-        priority_queue<double, vector<double>, greater<double>> pq;
-
+        vector<double> arrival;
         int n = dist.size();
+
         for(int i = 0;i < n;i++){
-            double timeToReach = (double)dist[i]/speed[i];
-            pq.push(timeToReach);
+            arrival.push_back((float) dist[i]/speed[i]);
         }
 
+        sort(arrival.begin(), arrival.end());
+
         int ans = 0;
-        while(pq.size() > 0){
-            double time = pq.top();
-            pq.pop();
 
-            if(t < time) ans++;
-            else return ans;
+        for(int i = 0;i < n;i++){
+            if(i >= arrival[i]){
+                break;
+            }
 
-            t += 1;
+            ans++;
         }
 
         return ans;
+
+        // Using Priority Queue.
+        // double t = 0;
+        // priority_queue<double, vector<double>, greater<double>> pq;
+
+        // int n = dist.size();
+        // for(int i = 0;i < n;i++){
+        //     double timeToReach = (double)dist[i]/speed[i];
+        //     pq.push(timeToReach);
+        // }
+
+        // int ans = 0;
+        // while(pq.size() > 0){
+        //     double time = pq.top();
+        //     pq.pop();
+
+        //     if(t < time) ans++;
+        //     else return ans;
+
+        //     t += 1;
+        // }
+
+        // return ans;
     }
 };
