f3bd5061e91f8442f92d313bdc533cc66b9a96c4
AdityaPatil-AP <aditya.patil20@vit.edu>
2023-12-10 10:59:46 +0530

    commit

--- Leet_Code_Solutions/String/1662.check-if-two-string-arrays-are-equivalent.cpp
+++ Leet_Code_Solutions/String/1662.check-if-two-string-arrays-are-equivalent.cpp
@@ -1,10 +1,14 @@
-class Solution {
+class Solution
+{
 public:
-    bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
+    bool arrayStringsAreEqual(vector<string> &word1, vector<string> &word2)
+    {
         string cum1, cum2;
 
-        for(int i = 0;i < word1.size();i++) cum1 += word1[i];
-        for(int i = 0;i < word2.size();i++) cum2 += word2[i];
+        for (int i = 0; i < word1.size(); i++)
+            cum1 += word1[i];
+        for (int i = 0; i < word2.size(); i++)
+            cum2 += word2[i];
 
         return (cum1 == cum2);
     }
