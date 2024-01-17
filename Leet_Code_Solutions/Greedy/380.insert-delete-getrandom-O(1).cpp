115c80be6cea72c9e2cd91f743f731bc648d7339
AdityaPatil-AP <aditya.patil20@vit.edu>
2024-01-17 11:03:06 +0530

    commit

--- Leet_Code_Solutions/Greedy/380.insert-delete-getrandom-O(1).cpp
+++ Leet_Code_Solutions/Greedy/380.insert-delete-getrandom-O(1).cpp
@@ -38,13 +38,3 @@ public:
         return arr[rand() % arr.size()];
     }
 };
-
-/**
- * @brief
- *
- */
-*RandomizedSet *obj = new RandomizedSet();
-*bool param_1 = obj->insert(val);
-*bool param_2 = obj->remove(val);
-*int param_3 = obj->getRandom();
-* /
