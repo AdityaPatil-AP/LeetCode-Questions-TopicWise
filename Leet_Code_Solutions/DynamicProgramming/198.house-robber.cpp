b86237cedbde7cdd0a93973fe475306e69290e13
AdityaPatil-AP <aditya.patil20@vit.edu>
2024-01-28 12:40:18 +0530

    commit

--- Leet_Code_Solutions/DynamicProgramming/198.house-robber.cpp
+++ Leet_Code_Solutions/DynamicProgramming/198.house-robber.cpp
@@ -44,16 +44,19 @@ public:
     }
 };
 
-class Solution {
+class Solution
+{
 public:
-    int rob(vector<int>& nums) {
+    int rob(vector<int> &nums)
+    {
         int ans = nums[0];
         int n = nums.size();
         vector<int> dp(n + 1);
         dp[0] = 0;
         dp[1] = nums[0];
 
-        for(int i = 2;i <= n;i++){
+        for (int i = 2; i <= n; i++)
+        {
             dp[i] = max(dp[i - 1], nums[i - 1] + dp[i - 2]);
         }
 
