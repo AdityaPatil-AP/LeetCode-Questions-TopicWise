1a5a47483ff3f35a9172526924bb3e1c99f39eae
AdityaPatil-AP <aditya.patil20@vit.edu>
2024-01-28 13:28:59 +0530

    commit

--- Leet_Code_Solutions/PrefixSum/1074.number-of-submatrices-that-sum-to-target.cpp
+++ Leet_Code_Solutions/PrefixSum/1074.number-of-submatrices-that-sum-to-target.cpp
@@ -3,39 +3,81 @@ class Solution
 public:
     int numSubmatrixSumTarget(vector<vector<int>> &matrix, int target)
     {
-        // 1) Find Prefix Sum for each row.
-        // 2) For each pair of column will find the subarrays in vertical fashion
-        // whose sum is equal to target.
+        // Using Prefix Sum
+        // Find Prefix Sum of each row.
+        // For each pair of columns, check if submatrices created by them sum
+        // to target.
 
-        int m = matrix.size();
-        int n = matrix[0].size();
+        // int n = matrix.size();
+        // int m = matrix[0].size();
 
-        for (int row = 0; row < m; row++)
-        {
-            for (int col = 1; col < n; col++)
-            {
-                matrix[row][col] += matrix[row][col - 1];
-            }
-        }
+        // for(int i = 0;i < n;i++){
+        //     for(int j = 1;j < m;j++){
+        //         matrix[i][j] += matrix[i][j - 1];
+        //     }
+        // }
+
+        // int count = 0;
+        // for(int c1 = 0;c1 < m;c1++){
+        //     for(int c2 = c1;c2 < m;c2++){
+        //         unordered_map<int,int> mp;
+        //         mp[0] = 1;
+        //         long long sum = 0;
+
+        //         for(int row = 0;row < n;row++){
+        //             sum += (matrix[row][c2] - (c1 > 0 ? matrix[row][c1 - 1] : 0));
+        //             count += mp[sum - target];
+        //             mp[sum]++;
+        //         }
+        //     }
+        // }
+
+        // return count;
+
+        // Using Subarray Sum
+        int n = matrix.size();
+        int m = matrix[0].size();
 
         int count = 0;
 
-        for (int c1 = 0; c1 < n; c1++)
+        for (int i = 0; i < n; i++)
         {
-            for (int c2 = c1; c2 < n; c2++)
+            vector<int> sum(m, 0);
+
+            for (int j = i; j < n; j++)
             {
-                unordered_map<int, int> mp;
-
-                mp[0] = 1;
-                int sum = 0;
-
-                for (int row = 0; row < m; row++)
+                for (int col = 0; col < m; col++)
                 {
-                    sum += matrix[row][c2] - (c1 > 0 ? matrix[row][c1 - 1] : 0);
-                    count += mp[sum - target];
-                    mp[sum]++;
+                    sum[col] += matrix[j][col];
                 }
+                count += subarraySum(sum, target);
             }
+        }
+
+        return count;
+    }
+
+    int subarraySum(vector<int> nums, int target)
+    {
+        int curr_sum = 0;
+        unordered_map<int, int> mp;
+        int count = 0;
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            curr_sum += nums[i];
+
+            if (curr_sum == target)
+            {
+                count++;
+            }
+
+            if (mp.find(curr_sum - target) != mp.end())
+            {
+                count += mp[curr_sum - target];
+            }
+
+            mp[curr_sum]++;
         }
 
         return count;
