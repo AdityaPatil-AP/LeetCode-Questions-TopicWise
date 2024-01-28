d425e794ee11fae4a50b6c09adb0e75e8e0990f6
AdityaPatil-AP <aditya.patil20@vit.edu>
2024-01-28 19:20:22 +0530

    commit

--- Leet_Code_Solutions/DynamicProgramming/1143.longest-common-subsequence.cpp
+++ Leet_Code_Solutions/DynamicProgramming/1143.longest-common-subsequence.cpp
@@ -1,24 +1,33 @@
-class Solution {
+class Solution
+{
 public:
-   
-    int longestCommonSubsequence(string text1, string text2) {
-        int m=text1.size(),n=text2.size();
-        int dp[m+1][n+1];
-        for(int i=0;i<m+1;i++){
-            dp[i][0]=0;
-        }
-        for(int i=0;i<n+1;i++){
-            dp[0][i]=0;
-        }
-        for(int i=1;i<m+1;i++){
-            for(int j=1;j<n+1;j++){
-               if(text1[i-1]==text2[j-1]){
-                  dp[i][j]= 1+dp[i-1][j-1];
-               }else{
-                   dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
-               } 
+    int longestCommonSubsequence(string text1, string text2)
+    {
+        int n = text1.size();
+        int m = text2.size();
+
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+        // This indicates empty string.
+        dp[n][m] = 0;
+
+        // dp[i][j] -> denotes the longest common subsequence for substring of text1[i.. n - 1] and
+        // text2[j... m - 1].
+
+        for (int i = n - 1; i >= 0; i--)
+        {
+            for (int j = m - 1; j >= 0; j--)
+            {
+                if (text1[i] == text2[j])
+                {
+                    dp[i][j] = 1 + dp[i + 1][j + 1];
+                }
+                else
+                {
+                    dp[i][j] = max(dp[i + 1][j], dp[i][j + 1]);
+                }
             }
         }
-        return dp[m][n];
+
+        return dp[0][0];
     }
 };
