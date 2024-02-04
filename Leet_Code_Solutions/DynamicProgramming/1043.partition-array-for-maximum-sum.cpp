7d3b96430412bfa6097e65862f4aed33a479c7bc
AdityaPatil-AP <aditya.patil20@vit.edu>
2024-02-04 23:34:34 +0530

    commit

--- Leet_Code_Solutions/DynamicProgramming/1043.partition-array-for-maximum-sum.cpp
+++ Leet_Code_Solutions/DynamicProgramming/1043.partition-array-for-maximum-sum.cpp
@@ -1,6 +1,8 @@
-class Solution {
+class Solution
+{
 public:
-    int maxSumAfterPartitioning(vector<int>& arr, int k) {
+    int maxSumAfterPartitioning(vector<int> &arr, int k)
+    {
         int n = arr.size();
         int dp[n];
 
@@ -9,20 +11,24 @@ public:
         return maxSum(arr, k, dp, 0);
     }
 
-    int maxSum(vector<int> &arr, int k, int dp[], int start){
+    int maxSum(vector<int> &arr, int k, int dp[], int start)
+    {
         int n = arr.size();
 
-        if(start >= n){
+        if (start >= n)
+        {
             return 0;
         }
 
-        if(dp[start] != -1){
+        if (dp[start] != -1)
+        {
             return dp[start];
         }
 
         int ans = 0;
         int currMax = arr[start];
-        for(int i = start;i < min(n, start + k);i++){
+        for (int i = start; i < min(n, start + k); i++)
+        {
             currMax = max(currMax, arr[i]);
 
             ans = max(ans, (i - start + 1) * currMax + maxSum(arr, k, dp, i + 1));
