a85671857aace60e64037a8227f09b1c2d7f25c0
AdityaPatil-AP <aditya.patil20@vit.edu>
2024-02-05 22:41:10 +0530

    commit

---
+++ Leet_Code_Solutions/String/387.first-unique-character-in-a-string.cpp
@@ -0,0 +1,19 @@
+class Solution
+{
+public:
+    int firstUniqChar(string s)
+    {
+        unordered_map<int, int> mp;
+
+        for (auto it : s)
+            mp[it]++;
+
+        for (int i = 0; i < s.size(); i++)
+        {
+            if (mp[s[i]] == 1)
+                return i;
+        }
+
+        return -1;
+    }
+};
