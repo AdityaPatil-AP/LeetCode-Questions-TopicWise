0041b0ef7f48d6389888a8e7f0b8197a4997822f
AdityaPatil-AP <aditya.patil20@vit.edu>
2024-02-08 22:35:28 +0530

    commit

--- Leet_Code_Solutions/DynamicProgramming/279.perfect-squares.cpp
+++ Leet_Code_Solutions/DynamicProgramming/279.perfect-squares.cpp
@@ -1,38 +1,48 @@
-class Solution {
+class Solution
+{
 public:
-    int solve(int n, vector<int> &dp){
-        if(n == 0){
-            return 0;
-        }
-        if(dp[n] != -1){
-            return dp[n];
-        }
-        int min_steps = INT_MAX;
-        for(int i = 1;i * i <= n;i++){
-            min_steps = min(min_steps, 1 + solve(n - i * i, dp));
-        }
-        
-        return dp[n] = min_steps;
-    }
-    
-    int numSquares(int n) {
-        // Recursion Based approach - divide and conquer. 
-        // Top - Down Approach.
-        // Memoisation.
-        // vector<int> dp(n + 1, -1);
-        // return solve(n, dp);
-        
-        
-        // Using Bottom Up Approach.
-        vector<int> dp(n + 1, INT_MAX);
+    int numSquares(int n)
+    {
+        // int dp[n + 1];
+
+        // memset(dp, -1, sizeof(dp));
+
+        // return solve(dp, n);
+
+        // bottom up approach.
+
+        vector<int> dp(n + 1, 1e9);
+
         dp[0] = 0;
-        for(int sq = 1;sq <= n;sq++){
-            // sq is the number we have to check.
-            for(int i = 1;i * i <= sq;i++){
-                dp[sq] = min(dp[sq], 1 + dp[sq - i*i]);
+
+        for (int i = 1; i <= n; i++)
+        {
+
+            for (int j = 1; j * j <= i; j++)
+            {
+                dp[i] = min(dp[i], 1 + dp[i - j * j]);
             }
         }
-        
+
         return dp[n];
+    }
+
+    int solve(int dp[], int i)
+    {
+        if (i == 0)
+            return 0;
+
+        if (dp[i] != -1)
+            return dp[i];
+
+        int ans = 1e9;
+
+        for (int j = 1; j * j <= i; j++)
+        {
+            int curr = j * j;
+            ans = min(ans, 1 + solve(dp, i - curr));
+        }
+
+        return dp[i] = ans;
     }
 };
