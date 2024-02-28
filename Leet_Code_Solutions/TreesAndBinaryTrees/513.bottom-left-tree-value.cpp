0640c3560a1744f53d9291f9a7fc7b14b4af125c
AdityaPatil-AP <aditya.patil20@vit.edu>
2024-02-28 10:26:55 +0530

    commit

---
+++ Leet_Code_Solutions/TreesAndBinaryTrees/513.bottom-left-tree-value.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int findBottomLeftValue(TreeNode* root) {
+        // Using bfs.
+
+        int ans = root->val;
+
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while(!q.empty()){
+            ans = q.front()->val;
+            int n = q.size();
+            for(int i = 0;i < n;i++){
+                TreeNode* curr = q.front();
+                q.pop();
+
+                if(curr->left) q.push(curr->left);
+                if(curr->right) q.push(curr->right);
+            }
+        }    
+
+        return ans;    
+    }
+};
